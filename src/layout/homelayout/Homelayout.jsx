ee54bb8252ecab539c2b4ec2d9589977eec8c6fb
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-10-19 22:22:56 +0600

    marquee added

--- src/layout/homelayout/Homelayout.jsx
+++ src/layout/homelayout/Homelayout.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import Header from '../../component/header/Header';
-import { Outlet } from 'react-router';
+import { NavLink, Outlet } from 'react-router';
+import Marquee from 'react-fast-marquee';
 const Homelayout = () => {
     return (
-        <div>
-          <Header></Header>
+        <div className='max-w-[1400px] mx-auto'>
+          <header>
+              <Header></Header>
+              <section className='flex items-center text-center bg-base-300 p-4 gap-5'>
+                <p className='bg-secondary text-white p-3'>Latest</p>
+              <Marquee pauseOnHover={true} direction='right'>
+                <p>Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</p>
+              </Marquee>
+              </section>
+              <section>
+                <nav>
+                  <NavLink>Home</NavLink>
+                  <NavLink>About</NavLink>
+                  <NavLink>Career</NavLink>
+                </nav>
+              </section>
+          </header>
+         
 
           <main>
             <section className='left_nav'></section>
